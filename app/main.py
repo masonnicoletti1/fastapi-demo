21bb10d271210cc344bca76fd516d699152eea67
Mason Nicoletti <mason.f.nicoletti@gmail.com>
2024-11-13 00:44:39 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -42,3 +42,37 @@ DB = "cxx6sw"
 
 db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
 cur = db.cursor()
+
+@app.route('/genres', methods=['GET'], cors=True)
+def get_genres():
+    query = "SELECT * FROM genres ORDER BY genreid;"
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        output = json.dumps(json_data)
+        return(output)
+    except mysql.connector.Error as e:
+        print("MySQL Error: ", str(e))
+        return None
+    cur.close()
+
+@app.route('/songs', methods=['GET'], cors=True)
+def get_songs():
+    query = "SELECT songs.title, songs.album, songs.artist, songs.year, songs.file, songs.image, genres.genre FROM songs JOIN genres ON songs.genre=genres.genreid ORDER BY songs.id;"
+    try:    
+        cur.execute(query)
+        headers=[x[0] for x in cur.description]
+        results = cur.fetchall()
+        json_data=[]
+        for result in results:
+            json_data.append(dict(zip(headers,result)))
+        output = json.dumps(json_data)
+        return(output)
+    except mysql.connector.Error as e:
+        print("MySQL Error: ", str(e))
+        return None
+    cur.close()
