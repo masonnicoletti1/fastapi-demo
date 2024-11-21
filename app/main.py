e08b4a1cccc059194fdb4c837f6341fcd4c23fbd
Mason Nicoletti <mason.f.nicoletti@gmail.com>
2024-11-21 11:09:53 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -42,9 +42,6 @@ DBUSER = "admin"
 DBPASS = os.getenv('DBPASS')
 DB = "cxx6sw"
 
-db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
-cur=db.cursor()
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -54,6 +51,8 @@ app.add_middleware(
 
 @app.get('/genres')
 def get_genres():
+    db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
+    cur=db.cursor()
     query = "SELECT * FROM genres ORDER BY genreid;"
     try:    
         cur.execute(query)
@@ -65,9 +64,14 @@ def get_genres():
         return(json_data)
     except Error as e:
         return {"Error": "MySQL Error: " + str(e)}
+    finally:
+        cur.close()
+        db.close()
 
 @app.get('/songs')
 def get_songs():
+    db = mysql.connector.connect(user=DBUSER, host=DBHOST, password=DBPASS, database=DB)
+    cur=db.cursor()
     query = "SELECT songs.title, songs.album, songs.artist, songs.year, songs.file, songs.image, genres.genre FROM songs JOIN genres ON songs.genre=genres.genreid ORDER BY songs.id;"
     try:    
         cur.execute(query)
@@ -79,3 +83,6 @@ def get_songs():
         return(json_data)
     except Error as e:
         return {"Error": "MySQL Error: " + str(e)}
+    finally:
+        cur.close()
+        db.close()
